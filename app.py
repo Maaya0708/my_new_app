a28652d4bb84bcfd863aec812080dbcaf4369d9a
Maaya0708 <121269812+Maaya0708@users.noreply.github.com>
2023-01-18 01:08:13 +0000

    1st

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
 import streamlit as st
-st.title("Hello, Streamlit!")
-st.write("こんにちは、Streamlit!")
+
+st.title("計算機")
+st.write("ここでは")
+
+num1 = st.number_input("Enter a number: ")
+num2 = st.number_input("Enter another number: ")
+
+result = ""
+
+if st.button("Add"):
+    result = num1 + num2
+elif st.button("Subtract"):
+    result = num1 - num2
+elif st.button("Multiply"):
+    result = num1 * num2
+elif st.button("Divide"):
+    result = num1 / num2
+
+st.write("Result: ", result)
+アプリを実行します。streamlit run [ファイル名].py
+
+ブラウザでアプリにアクセスします。http://localhost:8501/
+
+"Simple Calculator"というタイトルが表示され、2つの数字を入力するための入力ボックスが表示されます。加算、減算、乗算、除算のいずれかのボタンをクリックして、結果を計算します。
+
+これは簡
+
+
+Stop generating
