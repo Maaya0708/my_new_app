c087c17b9e536f4b1eca5057937ee583d7a9445e
Maaya0708 <121269812+Maaya0708@users.noreply.github.com>
2023-01-18 01:17:11 +0000

    1st

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
 import streamlit as st
 
-st.title("計算機")
-st.write("ここでは簡単な計算をします")
+st.title("Simple Web App")
 
-num1 = st.number_input("Enter a number: ")
-num2 = st.number_input("Enter another number: ")
+name = st.text_input("What's your name?")
 
-result = ""
-
-if st.button("Add"):
-    result = num1 + num2
-elif st.button("Subtract"):
-    result = num1 - num2
-elif st.button("Multiply"):
-    result = num1 * num2
-elif st.button("Divide"):
-    result = num1 / num2
-
-st.write("Result: ", result)
-
-
-Stop generating
+if st.button("Greet"):
+    result = "Hello, " + name
+    st.success(result)
