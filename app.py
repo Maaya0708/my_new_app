214cca27a38f77c4ddb3b5d53145d2c503f5e18c
Maaya0708 <121269812+Maaya0708@users.noreply.github.com>
2023-01-18 01:38:08 +0000

    1st

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ name = st.text_input("What's your name?")
 if st.button("Greet"):
     result = "Hello, " + name
     st.success(result)
+
+
+st.title("Simple Calculator")
+
+num1 = st.number_input("Enter a number: ")
+num2 = st.number_input("Enter another number: ")
+
+result = ""
+
+if st.button("Add"):
+    result = num1 + num2
+elif st.button("Subtract"):
+    result = num1 - num2
+elif st.button("Multiply"):
+    result = num1 * num2
+elif st.button("Divide"):
+    result = num1 / num2
+
+st.write("Result: ", result)
