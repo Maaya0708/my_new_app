49a24dc3306b5cd53750cd16a7638b1abf7370e9
Maaya0708 <121269812+Maaya0708@users.noreply.github.com>
2023-01-18 03:09:55 +0000

    1st

--- app.py
+++ app.py
@@ -15,9 +15,39 @@ st.write("何をしにこのサイトに来ましたか？")
 st.checkbox("簡単な計算をして欲しい")
 st.checkbox("翻訳をして欲しい")
 
-check=st.checkbox("簡単な計算をして欲しい")
-if check:
-    st.title("簡単な計算")
-    st.write("ここでは簡単な計算をします")
+st.title("簡単な計算")
+st.write("ここでは簡単な計算をします")
 
+num1 = st.number_input("Enter a number: ")
+num2 = st.number_input("Enter another number: ")
+
+result = ""
+    
+if st.button("Add"):
+    result = num1+ num2
+elif st.button("Subtract"):
+    result = num1 - num2
+elif st.button("Multiply"):
+    result = num1 * num2
+elif st.button("Divide"):
+    result = num1 / num2
+
+st.write("Result: ", result)
+    
+from googletrans import Translator
+st.set_page_config(page_title="翻訳アプリ", page_icon=":guardsman:", layout="wide")
+st.title("翻訳アプリ")
+
+   # 入力されたテキストを取得
+text = st.text_input("翻訳したいテキストを入力してください")
+
+   # 翻訳先の言語を選択
+languages = ["ja", "en", "fr", "es"]
+language = st.selectbox("翻訳先の言語を選択してください", languages)
+
+     # 翻訳ボタンを押した時の処理
+if st.button("翻訳"):
+    translator = Translator()
+    result = translator.translate(text, dest=language)
+    st.success(result.text)
     
